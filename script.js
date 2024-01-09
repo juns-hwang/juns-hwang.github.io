58f8c74f7aa08cf9f043f19b98a5f6b6706781e7
junh0921 <jun.hwang@berkeley.edu>
2024-01-09 19:11:46 +0900

    added about section, edited spacing for mobile view

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ function toggleMenu() {
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+function hideText() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
