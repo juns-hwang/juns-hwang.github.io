9e5dd593942f1f77ea54f3a17f7b007eef4ce587
junh0921 <jun.hwang@berkeley.edu>
2024-01-09 19:19:22 +0900

    fixed button answer showing by default

--- script.js
+++ script.js
@@ -7,9 +7,5 @@ function toggleMenu() {
 
 function hideText() {
     var x = document.getElementById("myDIV");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
+    x.style.display = x.style.display === 'block' ? 'none' : 'block';
+}
